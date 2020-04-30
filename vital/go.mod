74687d553c27cef8bfa8a82b33f44e0c53b1e093
Blake Watters <blake@opsani.com>
2020-04-30 01:56:22 -0700

    Support init via a token

--- vital/go.mod
+++ vital/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gofiber/fiber v1.9.3
 	github.com/klauspost/compress v1.10.5 // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- vital/main.go
+++ vital/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
+
 	"github.com/gofiber/fiber"
+	"gopkg.in/yaml.v2"
 )
+
+// ClientProfile is a configuration for an Opsani client
+type ClientProfile struct {
+	InitToken string `yaml:"init_token"`
+	BaseURL   string `yaml:"base_url"`
+	AppID     string `yaml:"app_id"`
+	APIToken  string `yaml:"api_token"`
+}
+
+// AppConfig represents data from the .config.yaml file
+type AppConfig struct {
+	// ProfilesByToken is a map of single use tokens to client profiles
+	Profiles []ClientProfile `yaml:"profiles"`
+}
 
 func main() {
 	app := fiber.New()
@@ -12,10 +28,8 @@ func main() {
 	// Serve static assets
 	app.Static("/", "./assets")
 
-	// TODO: Index serving static files
-	// builds, index, script
 	app.Post("/signup", func(c *fiber.Ctx) {
-		fmt.Printf("Hello! name=%q, email=%q, app=%q", 
+		fmt.Printf("Hello! name=%q, email=%q, app=%q",
 			c.FormValue("name"), c.FormValue("email"), c.FormValue("app_name"))
 	})
 
@@ -26,10 +40,35 @@ func main() {
 
 	// 	fmt.Printf("Token: %s", c.Params("token"))
 	// })
-	app.Get("/token/:token", func(c *fiber.Ctx) {
-		// Get the query param
-		fmt.Printf("Token is %q", c.Params("token"))
-	})	
+	app.Get("/init/:token", func(c *fiber.Ctx) {
+		config := AppConfig{}
+		data, err := ioutil.ReadFile(".config.yaml")
+		if err != nil {
+			panic(err)
+		}
+		err = yaml.Unmarshal([]byte(data), &config)
+		if err != nil {
+			panic(err)
+		}
+
+		var profile *ClientProfile
+		for _, p := range config.Profiles {
+			if p.InitToken == c.Params("token") {
+				profile = &p
+			}
+		}
+
+		if profile != nil {
+			c.JSON(fiber.Map{
+				"base_url": profile.BaseURL,
+				"app":      profile.AppID,
+				"token":    profile.APIToken,
+			})
+		} else {
+			c.Send("Unknown token")
+			c.SendStatus(404)
+		}
+	})
 
 	app.Listen(8080)
 }
