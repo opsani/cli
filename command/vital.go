eb7179e9ef7a225625ce5bcbb65840e54cdd9edf
Blake Watters <blake@opsani.com>
2020-05-27 19:47:32 -0700

    Move timeout up to 5 mins

--- command/vital.go
+++ command/vital.go
@@ -443,7 +443,7 @@ func (vitalCommand *vitalCommand) InstallKubernetesManifests(cobraCmd *cobra.Com
 		Failure:     "failed waiting for prometheus pod",
 		Run: func() error {
 			outcome := make(chan error)
-			ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 			defer cancel()
 			go func() {
 				for {
